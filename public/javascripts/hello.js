c21fadd2dd2f1d4929d4ce1071b917cf4c8e6028
MrFabkirox <fabrice.esope@gmail.com>
2014-11-04 13:54:58 +0000

    first commit

---
+++ public/javascripts/hello.js
@@ -0,0 +1,13 @@
+if (window.console) {
+  console.log("Welcome to your Play application's JavaScript!");
+}
+
+
+function myFunction() {
+	document.getElementById("demo").innerHTML = "After, Paragraph changed";
+}
+
+function truc3() {
+	document.alert('truc');
+}
+
